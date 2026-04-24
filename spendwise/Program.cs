7cc424a23e034e4c72299606ff89540e89c482e9
Muhammad Haris Javed <71623701+harisjaved1996@users.noreply.github.com>
2026-04-24 11:22:49 +0500

    connection established successfully and add files in .gitignore

--- spendwise/Program.cs
+++ spendwise/Program.cs
@@ -1,6 +1,12 @@
+using spendwise.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddControllersWithViews()
     .AddRazorRuntimeCompilation();
 
